359ab3953ecae362bfae5dc4466f15684a508915
= <=>
2015-04-17 11:40:57 -0400

    Create session controller and helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :log_in_user!, :logged_in?
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def log_in_user!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by_credentials(
+      params[:user][:collar],
+      params[:user][:password]
+    )
+
+    if user.nil?
+      render :new
+    else
+      log_in_user!(user)
+      redirect_to user_url(user)
+    end
+  end
+
+  def new
+    render :new
+  end
+
+  def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+
+    redirect_to #root
+  end
+end
